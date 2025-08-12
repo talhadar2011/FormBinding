9b222f7cb5443d3c60bfdcf96bcc3a4d4b91b234
Talha <talhadar2011@gmail.com>
2025-08-12 12:41:14 +0200

    Setting Up

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1> Web Form</h1>
+  <SignpForm/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import SignpForm from './components/SignpForm.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SignpForm
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+ margin: 0;
+ text-align: center;
 }
 </style>
---
+++ src/components/SignpForm.vue
@@ -0,0 +1,16 @@
+<template>
+  <form>
+    <label>Email:</label>
+    <input type="email" placeholder="Enter you E-Mail Address">
+  </form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
