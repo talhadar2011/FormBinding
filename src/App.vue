097fcdf0ce0c7404e9878c4821c5ffd55736e665
Talha <talhadar2011@gmail.com>
2025-08-12 12:52:12 +0200

    Form Component Implemented

--- src/App.vue
+++ src/App.vue
@@ -14,8 +14,16 @@ export default {
 </script>
 
 <style>
-#app {
- margin: 0;
- text-align: center;
+#app { 
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body{
+  margin: 0px;
+  background-color: #eee;
 }
 </style>
